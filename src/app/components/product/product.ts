65abf5df6dc39f0c9fb5405f6de7b61ac2111519
Myhtafa_break <sum686431@gmail.com>
2026-08-11 16:33:38 +0300

    Kategori listeleme butonlarina basilinca urun goruntuleme eklendi

--- src/app/components/product/product.ts
+++ src/app/components/product/product.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/Product';
 import { ProductService } from '../../services/product';
 import { ListResponseModel } from '../../models/listResponseModel';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -15,13 +16,25 @@ export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params => {
+      if (params["categoryId"]) {
+        this.getProductsByCategory(params["categoryId"]);
+      } else {
+        this.getProducts();
+      }
+    })
   }
 
   getProducts() {
     this.productService.getProducts().subscribe(response => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    })
+  }
+  getProductsByCategory(categoryId: number) {
+    this.productService.getProductsByCategory(categoryId).subscribe(response => {
       this.products = response.data;
       this.dataLoaded = true;
     })
